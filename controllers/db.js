f305abec9e35c0ad1f6ac232c6a2b0bc659653ac
Faus-MSI <faausc@gmail.com>
2025-01-06 12:27:31 -0300

    Actualizo package y db nube

--- controllers/db.js
+++ controllers/db.js
@@ -1,11 +1,19 @@
 // controllers/db.js
 const mysql = require('mysql2');
 
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '251199',
+//     database: 'gootpv', 
+// });
+
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '251199',
-    database: 'db_tienda'
+    host: 'bsjulmyhtq0msiqavyc9-mysql.services.clever-cloud.com',
+    user: 'u6akbkeycp6pdtu8',
+    password: 'Krz6XnBJNxvWH86I07BH',
+    database: 'bsjulmyhtq0msiqavyc9', 
 });
 
 db.connect(err => {
