c9e2ed18009856ab1aa78ea22e0a47242458b849
Faus-MSI <faausc@gmail.com>
2025-01-06 20:30:35 -0300

    bd nube

--- controllers/db.js
+++ controllers/db.js
@@ -1,20 +1,20 @@
 // controllers/db.js
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '251199',
-    database: 'gootpv', 
-});
-
-
 // const db = mysql.createConnection({
-//     host: 'bsjulmyhtq0msiqavyc9-mysql.services.clever-cloud.com',
-//     user: 'u6akbkeycp6pdtu8',
-//     password: 'Krz6XnBJNxvWH86I07BH',
-//     database: 'bsjulmyhtq0msiqavyc9', 
+//     host: 'localhost',
+//     user: 'root',
+//     password: '251199',
+//     database: 'gootpv', 
 // });
+
+
+const db = mysql.createConnection({
+    host: 'bsjulmyhtq0msiqavyc9-mysql.services.clever-cloud.com',
+    user: 'u6akbkeycp6pdtu8',
+    password: 'Krz6XnBJNxvWH86I07BH',
+    database: 'bsjulmyhtq0msiqavyc9', 
+});
 
 db.connect(err => {
     if (err) {
